9dfa8a75529e4a109e096f9b7ec380078e9f3e98
AUnicornWithNoLife <benjamin.barratt@icloud.com>
2020-10-26 11:00:29 +0000

    Update __main__.py

--- SRC/__main__.py
+++ SRC/__main__.py
@@ -2,12 +2,30 @@ print()
 print("Loading Petrol")
 print()
 
-try:
+#this launches the code without error prevention, THIS SHOULD ALWAYS BE FALSE IN RELEASE, BETA AND ALPHA VERSIONS
+DODGY_LAUNCH = False
+
+if DODGY_LAUNCH:
     import petrol
-except Exception as e:
-    print("ERROR")
-    print()
-    print(e.__str__())
-    print()
-    print("If this is an ongoing issue, please reinstall the software, then if it still happens report it on the Github repository. Thank you")
-    print()
+else:
+    load = "Y"
+
+    while load.upper() == "Y":
+        try:
+            import petrol
+        except Exception as e:
+            print("ERROR")
+            print()
+
+            print(e.__str__())
+
+            print()
+            print("If this is an ongoing issue, please reinstall the software, then if it still happens report it on the Github repository. Thank you")
+            print()
+
+            print("Do you want to reload? [Y/N]")
+            print()
+
+            load = input("? ")
+
+            print()
