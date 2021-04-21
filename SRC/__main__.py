e47419e740102c1e624fc8cb3f24bedaf05765c1
AUnicornWithNoLife <benjamin.barratt@icloud.com>
2021-04-21 17:58:39 +0100

    Update __main__.py

--- SRC/__main__.py
+++ SRC/__main__.py
@@ -3,43 +3,64 @@
 
 DODGY_LAUNCH = False #this launches the code without error prevention, THIS SHOULD ALWAYS BE FALSE IN RELEASE, BETA AND ALPHA VERSIONS
 
+
+DEBUG = False # adds option to reload scripts when keyboard interrupt given, should be false in all published versions
+
 def DODGY():
     print()
-    print("Launching Petrol, DODGY")
+    print("Launching Petrol, DODGY; ALL ERRORS WILL NOT BE RESOLVED")
     print()
 
     import petrol
 
-if DODGY_LAUNCH:
-    DODGY()
-else:
-    load = "Y"
+def launch():
+    try:
+        if DODGY_LAUNCH:
+            DODGY()
+        else:
+            load = "Y"
 
-    while load.upper() == "Y":
-        try:
+            while load.upper() == "Y":
+                try:
+                    print()
+                    print("Launching Petrol")
+                    print()
+
+                    import petrol
+
+                    load = "N"
+                except Exception as e:
+                    print("ERROR")
+                    print()
+
+                    print(e.__str__())
+
+                    print()
+                    print("If this is an ongoing issue, please reinstall the software, then if it still happens report it on the Github repository. Thank you")
+                    print()
+
+                    print("Do you want to reload? [Y/N]")
+                    print()
+
+                    load = input("? ")
+
+                    print()
+
+            if load.upper() == "DODGY":
+                DODGY()
+    except KeyboardInterrupt:
+        #catches terminal keybard interupt, so that you dont get an error message
+
+        if DEBUG:
+            if input("Do you want to relaunch? ").upper() == "Y":
+                launch()
+            else:
+                print()
+                print("Exiting...")
+                print()
+        else:
             print()
-            print("Launching Petrol")
+            print("Exiting...")
             print()
 
-            import petrol
-
-            load = "N"
-        except Exception as e:
-            print("ERROR")
-            print()
-
-            print(e.__str__())
-
-            print()
-            print("If this is an ongoing issue, please reinstall the software, then if it still happens report it on the Github repository. Thank you")
-            print()
-
-            print("Do you want to reload? [Y/N]")
-            print()
-
-            load = input("? ")
-
-            print()
-
-    if load.upper() == "DODGY":
-        DODGY()
+launch()
